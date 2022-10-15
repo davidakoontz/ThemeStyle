c61caa644feb71e060f3ff9e4cfcd349cba4b79d
davidakoontz <david@koontz.name>
2022-10-15 17:40:31 -0500

    Initial Commit

---
+++ ThemeStyle/ThemeStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeStyleApp.swift
+//  ThemeStyle
+//
+//  Created by David on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct ThemeStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
